de247cea9c7453af75674a253a86936a1ac0bead
Aluno vocação Gustavo <gustavo000923.sme@gmail.com>
2026-07-15 08:24:25 -0300

    Update

---
+++ modulo06/modulo06_intro_python_gustavo-ativ01.py
@@ -0,0 +1,20 @@
+'''
+
+Módulo 06 - Manipulação de arquivos em python
+neste módulo, eu irei fazer 3 exercícios de manipulação de arquivos em python
+
+'''
+
+
+conteudo_para_salvar = "Olá! Este é um texto de teste armazenado em um arquivo TXT usando Python."
+
+with open("dados.txt", "w", encoding="utf-8") as arquivo:
+    arquivo.write(conteudo_para_salvar)
+print("Arquivo TXT criado e salvo com sucesso!")
+
+
+with open("dados.txt", "r", encoding="utf-8") as arquivo:
+    conteudo_lido = arquivo.read()
+
+print("\n--- Conteúdo lido do arquivo TXT ---")
+print(conteudo_lido)
---
+++ modulo06/modulo06_intro_python_gustavo-ativ02.py
@@ -0,0 +1,24 @@
+'''
+
+Módulo 06 - Manipulação de arquivos em python
+neste módulo, eu irei fazer 3 exercícios de manipulação de arquivos em python
+
+'''
+
+import json
+
+clientes = {
+    "cliente_1": {"nome": "Carlos Souza", "email": "carlos@email.com", "ativo": True},
+    "cliente_2": {"nome": "Beatriz Lima", "email": "beatriz@email.com", "ativo": False}
+}
+
+with open("clientes.json", "w", encoding="utf-8") as arquivo_json:
+    # indent=4 serve para deixar o arquivo visualmente organizado ("bonito")
+    json.dump(clientes, arquivo_json, indent=4, ensure_ascii=False)
+print("Dicionário salvo em clientes.json!")
+
+with open("clientes.json", "r", encoding="utf-8") as arquivo_json:
+    dados_carregados = json.load(arquivo_json)
+
+print("\n--- Dados carregados do JSON ---")
+print(dados_carregados)
